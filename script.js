0f48f54d6fa41649cd35cc86faad93a164b08df3
Hamidreza <111283254+hamidrezaazinvand@users.noreply.github.com>
2023-09-17 23:03:06 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+
 function TD() {
     window.location.href = "https://hamidrezaazinvand.github.io/TowerDefence/";
 }
 function Tetris() {
     window.location.href = "https://hamidrezaazinvand.github.io/Tetris/Jar/Adventure.jar";
+}
+function FoxyTown() {
+    window.location.href = "foxytown.html"
+}
+function FoxyTown_1() {
+    window.location.href = "https://hamidrezaazinvand.github.io/FoxyTown/Foxy%20Town.part1.rar";
+}
+function FoxyTown_2() {
+    window.location.href = "https://hamidrezaazinvand.github.io/FoxyTown/Foxy%20Town.part2.rar";
 }
