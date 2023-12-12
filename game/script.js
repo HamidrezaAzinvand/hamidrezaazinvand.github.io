2c8fe0ed067aef9e65eff8168bb991675ecf9257
Hamidreza <111283254+hamidrezaazinvand@users.noreply.github.com>
2023-12-13 01:20:46 +0330

    Add files via upload

--- game/script.js
+++ game/script.js
@@ -6,7 +6,7 @@ function Tetris() {
     window.location.href = "https://hamidrezaazinvand.github.io/Tetris/Jar/Adventure.jar";
 }
 function FoxyTown() {
-    window.location.href = "downloads/foxytown.html"
+    window.location.href = "https://hamidrezaazinvand.github.io/game/downloads/foxytown.html"
 }
 function FoxyTown_1() {
     window.location.href = "https://hamidrezaazinvand.github.io/FoxyTown/Foxy%20Town.part1.rar";
@@ -19,4 +19,19 @@ function BackMain() {
 }
 function orangeBoy() {
     window.location.href = "https://hamidrezaazinvand.github.io/Orange-Boy-Adventure/Orange-Boy%20(Jar).rar";
+}
+function orangeBoyExe(i) {
+    window.location.href = "https://hamidrezaazinvand.github.io/Orange-Boy-Adventure/OrangeBoy.part0" + i + ".rar";
+}
+function testProject() {
+    window.location.href = "https://hamidrezaazinvand.github.io/game/downloads/test.html";
+}
+function bunnysValley() {
+    window.location.href = "https://hamidrezaazinvand.github.io/game/downloads/bunnysValley.html";
+}
+function bunnysValleyPart1() {
+    window.location.href = "https://hamidrezaazinvand.github.io/Bunnys-Valley/Bunny%20Valley.part1.rar";
+}
+function bunnysValleyPart2() {
+    window.location.href = "https://hamidrezaazinvand.github.io/Bunnys-Valley/Bunny%20Valley.part2.rar";
 }
