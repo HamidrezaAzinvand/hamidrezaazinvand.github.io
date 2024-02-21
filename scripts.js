f775ef43dbfa84cfc07a573efa09961f585b8d99
Hamidreza <111283254+m1ghtymeet@users.noreply.github.com>
2024-02-21 23:01:21 +0330

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function go_oba() {
+    window.location.href = "html/oba.html"
+}
+function download_oba(jar, part) {
+	if (jar != null && part == null) {
+		window.location.href = "https://github.com/m1ghtymeet/Orange-Boy-Adventure/" + jar + ".rar"
+	}
+}
+function download_tetris() {
+	window.location.href = "https://github.com/m1ghtymeet/Tetris/Jar/Adventure.jar"
+}
