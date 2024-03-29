064909b64b7d9588873d03a14de35331d0d2169b
Hamidreza <111283254+m1ghtymeet@users.noreply.github.com>
2024-03-30 00:00:47 +0330

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
 function go_oba() {
     window.location.href = "html/oba.html"
 }
+function go_plat() {
+	window.location.href = "html/plat.html"
+}
+function go_bunnyvalley() {
+	window.location.href = "html/bunnyvalley.html"
+}
+function download_plat() {
+	window.location.href = "html/plat.html"
+}
 function download_oba(jar, part) {
 	if (jar != null && part == null) {
-		window.location.href = "https://github.com/m1ghtymeet/Orange-Boy-Adventure/" + jar + ".rar"
+		window.location.href = "https://m1ghtymeet.github.io/Orange-Boy-Adventure/" + jar + ".rar"
 	}
 }
+function download_bunnyvalley(part) {
+	window.location.href = "https://m1ghtymeet.github.io/Bunnys-Valley/Bunny%20Valley.part" + part + ".rar"
+}
 function download_tetris() {
-	window.location.href = "https://github.com/m1ghtymeet/Tetris/Jar/Adventure.jar"
+	window.location.href = "https://m1ghtymeet.github.io/Tetris/Jar/Adventure.jar"
 }
